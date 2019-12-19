84f79238eebc1b90fc77020633afc57fb24d1a34
litichevskiy <kunin25@ukr.net>
2019-12-19 20:41:57 +0200

    Updated sw cache name, and add /dist

---
+++ dist/js/css.js
@@ -0,0 +1,1 @@
+webpackJsonp([1],{114:function(n,o,c){n.exports=c(115)},115:function(n,o){}},[114]);
--- src/js/sw.js
+++ src/js/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = ['v-1-1575726381980'];
+const CACHE_NAME = ['v-1-1576780521714'];
 
 const FILES_TO_CACHE = [
   '/',
